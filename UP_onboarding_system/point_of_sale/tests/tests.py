dd0a74f4a642bcf0bf34cffe2b1715a13c6db6bc
Sudarshan Sinha <s.sinha2103@outlook.com>
2022-10-14 14:33:16 +0530

    added assert if the obj was created for the respective model with the details provided to tests.py

--- UP_onboarding_system/point_of_sale/tests/tests.py
+++ UP_onboarding_system/point_of_sale/tests/tests.py
@@ -43,11 +43,13 @@ def test_item_creation_api_endpoint(merchant_data):
     #import pdb; pdb.set_trace()
     response = client.post("/login/", dict(username="merchant", password="password@123"))
     token = response.data['access']
-    store = Stores.objects.create(name="WhiteField Outlet", address="WhiteField", lat=13, lng=13,
+    store = Stores.objects.create(name="Mc Donald's WhiteField Outlet", address="WhiteField", lat=13, lng=13,
                                   merchant=merchant_data)
-    req_send = client.post("/items/", dict(name="Dosa", price=120, description="Sambhar Dosa", stores=store.pk),
+    req_send = client.post("/items/", dict(name="Veg Burger", price=160, description="With Aloo Tikki", stores=store.pk),
                            **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
     assert req_send.status_code == 201
+    items_present = set(Items.objects.all().values_list('name', flat=True))
+    assert "Veg Burger" in items_present
 
 
 # consumer Login & Registration Test Case
@@ -68,9 +70,10 @@ def test_consumer_login(consumer_data):
 # Test Place Order Endpoints
 @pytest.mark.django_db
 def test_place_order_api_endpoints(merchant_data, consumer_data):
-    # import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
     response = client.post("/login/", dict(username="consumer", password="password@123"))
     token = response.data["access"]
+    orders_before = set(Orders.objects.all().values_list('merchant', flat=True))
     store = Stores.objects.create(name="WhiteField Outlet", address="WhiteField", lat=13, lng=13,
                                   merchant=merchant_data)
     item = Items.objects.create(name="Dosa", price=120, description="Sambhar Dosa", stores=store)
@@ -78,3 +81,5 @@ def test_place_order_api_endpoints(merchant_data, consumer_data):
                            dict(user=consumer_data, merchant=merchant_data.pk, store=store.pk, items=item.pk),
                            **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
     assert req_send.status_code == 201
+    orders_present = set(Orders.objects.all().values_list('merchant', flat=True))
+    assert len(orders_present)>len(orders_before)
